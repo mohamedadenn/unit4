4dd5569dc38eda242e359b9bdf216a0f9ea3dd9d
mohamed <mohamed.aden@live.ca>
2020-07-24 18:22:51 -0400

    Updates

---
+++ precache-manifest.3dec63a798b7081b91c85967f525fd73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b564cf4be2ec0df9fa35cfd9e340a7af",
+    "url": "/unit4/index.html"
+  },
+  {
+    "revision": "5c27276121aba214ec05",
+    "url": "/unit4/static/css/main.ee099989.chunk.css"
+  },
+  {
+    "revision": "91693b01a2945a60f959",
+    "url": "/unit4/static/js/2.2ba1d769.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unit4/static/js/2.2ba1d769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c27276121aba214ec05",
+    "url": "/unit4/static/js/main.04bc12f9.chunk.js"
+  },
+  {
+    "revision": "c0569e83225e5fc204ed",
+    "url": "/unit4/static/js/runtime-main.4b19408e.js"
+  }
+]);
